b0d594f94c538065e346211f4f3997f300bff3e1
Kkhap <Kkhap@jayshree1>
2023-07-30 23:29:38 +0530

    commiting third time

--- src/package2/Primenumber.java
+++ src/package2/Primenumber.java
@@ -13,17 +13,18 @@ public class Primenumber {
 
 		public static void main(String[] args) 
 		{
-			int num=101,prime=1;;
-			for(int v=2;v<num;v++)
+			int num=101;
+			int v;
+			for(v=2;v<num;v++)
 			{
 				if(num%v==0)
 				{
-					prime=0;
+				
 					break;
 				}
 			}
 			
-			if(prime==1)
+			if(v==num)
 			{
 				System.out.println("it is prime");  
 			}
